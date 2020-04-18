fa200906f32b0944b7e3850608cc09ff6877bf36
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-04-19 00:42:29 +0200

    cargo fmt & fix tests

--- examples/chunked.rs
+++ examples/chunked.rs
@@ -9,8 +9,12 @@ fn main() -> Result<(), std::io::Error> {
         let mut app = tide::new();
         app.at("/").get(|_| async move {
             let file = fs::File::open(file!()).await.unwrap();
+<<<<<<< HEAD
             let res = Response::new(StatusCode::Ok).body(BufReader::new(file));
             Ok(res)
+=======
+            Response::new(StatusCode::Ok).body(BufReader::new(file))
+>>>>>>> 5689596... cargo fmt & fix tests
         });
         app.listen("127.0.0.1:8080").await?;
         Ok(())
